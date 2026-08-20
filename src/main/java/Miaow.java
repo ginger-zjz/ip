10333ddc33e4ed41cdd37b8077d6f883339e7eb0
Ginger <ginger06.zhang@gmail.com>
2026-08-20 10:49:30 +0800

    echo

--- src/main/java/Miaow.java
+++ src/main/java/Miaow.java
@@ -1,14 +1,37 @@
+import java.util.Scanner;
+
 public class Miaow {
     public static void main(String[] args) {
-        /*String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";*/
         String banner = " _._     _,-'\"\"`-._\n" +
                 "(,-.`._,'(       |\\`-/|\n" +
                 "    `-.-' \\ )-`( , o o)    MIAOW\n" +
-                "          `-    \\`_`\"'-";
+                "          `-    \\`_`\"'-\n" +
+                "Hello! I'm Miaow.\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n";
         System.out.println(banner);
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            System.out.println("____________________________________________________________");
+
+            if (command.equals("bye")) {
+                System.out.println("Miaow: Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
+                break;
+            } else {
+                System.out.println("Miaow: " + command);
+            }
+
+            //System.out.println(" " + command);
+            System.out.println(
+                    "____________________________________________________________"
+            );
+        }
+        //
     }
 }
