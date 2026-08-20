8d1b1e42bc70826f1f9584e9ceec1932946e9e0d
Ginger <ginger06.zhang@gmail.com>
2026-08-20 10:25:47 +0800

    Added cat as chatbot

---
+++ src/main/java/Miaow.java
@@ -0,0 +1,14 @@
+public class Miaow {
+    public static void main(String[] args) {
+        /*String banner = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        String banner = " _._     _,-'\"\"`-._\n" +
+                "(,-.`._,'(       |\\`-/|\n" +
+                "    `-.-' \\ )-`( , o o)    MIAOW\n" +
+                "          `-    \\`_`\"'-";
+        System.out.println(banner);
+    }
+}
